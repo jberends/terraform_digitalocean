89b8d24da1197e33e4c78aff271af7ce60e6bfaa
Jochem Berends <jochem.berends@ke-works.com>
2022-06-01 21:46:34 +0200

    finished tutorial 5 of the DO tutorial; nowe learned about outputs and squat outputs suchs as [*] and used jq in a pipe command on the cli

---
+++ terraform-outputs/droplets.tf
@@ -0,0 +1,11 @@
+resource "digitalocean_droplet" "web" {
+  count  = 3
+  image  = "ubuntu-20-04-x64"
+  name   = "test-droplet-${count.index}"
+  region = "ams3"
+  size   = "s-1vcpu-1gb"
+}
+
+output "droplet_ip_addresses" {
+  value = digitalocean_droplet.web[*].ipv4_address
+}
