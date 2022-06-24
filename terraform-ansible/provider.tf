09982f04c4609ded378d2436a5b19bf0d186cfdd
Jochem Berends <jochem.berends@ke-works.com>
2022-06-24 17:58:33 +0200

    this was tutorial 6 of the terraform tutorial where we added modules and had a for loop to create many droplets and loadbalancers at the same time.

---
+++ terraform-ansible/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+
+variable "do_token" {}
+variable "pvt_key" {}
+variable "pub_key" {}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+data "digitalocean_ssh_key" "jochem_berends_feb22" {
+  name = "jochem_berends_feb22"
+}
